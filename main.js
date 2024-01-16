b0379155e207a709f40af9682d0b9cc7c3f946d2
Matthew <mdav121503@gmail.com>
2024-01-16 09:48:09 -0600

    add playRound function

--- main.js
+++ main.js
@@ -3,3 +3,49 @@ function getComputerChoice(){
     const choice = Math.floor(Math.random() * 3);
     return choices[choice]
 }
+//GET player input
+//IMPLEMENT a function that handles the game
+//  rock beats scissors
+//  scissors beats paper
+//  paper beats rock
+
+function playRound(playerSelection, computerChoice){
+     switch(playerSelection){
+        case "rock":
+            if (computerChoice==="rock"){
+                return 2;
+            }
+            else if (computerChoice==="paper"){
+                return 0;
+            }
+            else if (computerChoice==="scissors"){
+                return 1;
+            }
+            break;
+
+        case "paper":
+            if (computerChoice==="rock"){
+                return 1;
+            }
+            else if (computerChoice==="paper"){
+                return 2;
+            }
+            else if (computerChoice==="scissors"){
+                return 0;
+            }
+            break;
+
+        case "scissors":
+            if (computerChoice==="rock"){
+                return 0;
+            }
+            else if (computerChoice==="paper"){
+                return 1;
+            }
+            else if (computerChoice==="scissors"){
+                return 2;
+            }
+            break;
+
+     }
+}
